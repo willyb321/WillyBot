abd2e9f00d6acf8d100b6b67431b84d988a00637
willyb321 <wbwilliam7@gmail.com>
2019-04-04 12:11:22 +1100

    finish creategame command

--- src/commands/selfmod/creategame.ts
+++ src/commands/selfmod/creategame.ts
@@ -33,11 +33,53 @@ export class CreateGameCommand extends Commando.Command {
 	}
 
 	async run(msg: Commando.CommandoMessage, args: any) {
-		const name = `game-${args.name.replace('game-', '')}`;
-		let role = msg.guild.roles.find(e => e.name.toLowerCase() === name.toLowerCase());
-		if (role) {
-			return msg.reply('Game already exists');
+		const roleName = `game-${args.name.replace('game-', '')}`;
+		const channelName = `${args.name.replace('game-', '')}`;
+		let role: any = msg.guild.roles.find(e => e.name.toLowerCase() === roleName.toLowerCase());
+		let channel: any = msg.guild.channels.find(e => e.name.toLowerCase() === channelName.toLowerCase());
+		if (role && channel) {
+			return msg.reply('Game tag and channel already exists. Manually delete if needed.');
 		}
-
+		if (!role) {
+			const roleData = {
+				data: {
+					name: roleName,
+					permissions: 0,
+					mentionable: false
+				},
+				reason: `Game requested by ${msg.author.tag} [${msg.author.id}]`
+			};
+			try {
+				role = await msg.guild.roles.create(roleData);
+			} catch (e) {
+				console.error(e);
+				return msg.reply('Failed! Check permissions');
+			}
+		}
+		if (!channel) {
+			const channelData: any = {
+				type: 'text',
+				name: channelName,
+				parent: '563095678088380416',
+				permissionOverwrites: [
+					{
+						id: role.id,
+						allow: ['SEND_MESSAGES', 'READ_MESSAGES', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'EMBED_LINKS', 'ADD_REACTIONS', 'VIEW_CHANNEL']
+					},
+					{
+						id: msg.guild.roles.first().id,
+						deny: ['SEND_MESSAGES', 'READ_MESSAGES', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'EMBED_LINKS', 'ADD_REACTIONS', 'VIEW_CHANNEL']
+					}
+				],
+				reason: `Game requested by ${msg.author.tag} [${msg.author.id}]`
+			};
+			try {
+				channel = await msg.guild.channels.create(channelName, channelData);
+			} catch (e) {
+				console.error(e);
+				return msg.reply('Failed! Check permissions');
+			}
+		}
+		return msg.reply(`Game ${args.name} created ${channel.toString()}`);
 	}
 }
